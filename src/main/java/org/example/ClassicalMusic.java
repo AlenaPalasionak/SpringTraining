59a644b6b67a2dd507274540662a20a4a7bd7a28
Lena Palasionak <70293576+AlenaPalasionak@users.noreply.github.com>
2022-03-12 12:44:47 +0300

    Add files via upload
    Initial Commit

---
+++ src/main/java/org/example/ClassicalMusic.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Neil Alishev
+ */
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/org/example/RockMusic.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Neil Alishev
+ */
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
